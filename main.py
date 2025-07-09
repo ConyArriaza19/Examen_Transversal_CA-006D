964767d598c39d6cb50076ba736227bda6155b13
unknown <SSDD@PNAWSA05LC54411.duoc.acad>
2025-07-09 13:37:06 -0400

    Segundo commit

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("\nProbando primer commit")
+# === PROGRAMA PRINCIPAL === #
+import functions as func
+
+print("\n === ** Tienda Online Pybook ** ===")
+
+productos = {
+    "8475HD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
+    "2175HD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
+    "JjfFHD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
+    "fgdxFHD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
+    "GF75HD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
+    "123FHD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
+    "342FHD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
+    "UWU131HD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"]
+}
+
+stock = {
+    "8475HD" : [387990 , 10],
+    "2175HD" : [387990 , 10],
+    "JjfFHD" : [387990 , 10],
+    "GF75HD" : [387990 , 10],
+    "123FHD" : [387990 , 10],
+    "342FHD" : [387990 , 10],
+    "UWU131HD" : [387990 , 10]
+}
