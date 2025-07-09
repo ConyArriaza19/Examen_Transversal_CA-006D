ad09f63754c0b5a3b85d9cf584edac935e451863
unknown <SSDD@PNAWSA05LC54411.duoc.acad>
2025-07-09 15:22:38 -0400

    Archivos FINALES

--- functions.py
+++ functions.py
@@ -1,1 +1,71 @@
 # === LIBRERIA DE FUNCIONES === #
+
+# === Función para ver stock por marca === #
+def stock_marca(marca , productos , stock):
+
+    stock_total = 0
+
+    for producto in productos:
+
+        if(productos[producto][0] == marca):
+
+            for modelo in stock:
+
+                if (modelo == producto):
+
+                    stock_total += stock[modelo][1]
+
+    print(f"\nLa cantidad de stock total de la marca {marca} es: {stock_total}")
+
+# === Función para buscar por precios === #
+def busqueda_precio(p_min , p_max , stock , productos):
+
+    Modelos_seleccionados = []
+
+    for modelo in stock:
+
+        if (stock[modelo][0] >= p_min and stock[modelo][0] <= p_max and stock[modelo][1] != 0):
+
+            for producto in productos:
+
+                if (producto == modelo):
+
+                    Marca = productos[producto][0]
+
+                    Modelo = modelo
+
+                    Formato =Marca + "--" + Modelo
+            
+                    Modelos_seleccionados.append(Formato)
+
+    Modelos_seleccionados.sort()
+
+    if (Modelos_seleccionados == []):
+        
+        print("\nNo hay notebooks en ese rango de precios.")
+
+    else:
+
+        print("\nLos notebooks entre los precios consultados son: ", Modelos_seleccionados)
+
+# === Función para actualizar precios === #
+def actualizar_precio(modelo , p , stock):
+
+    actualizacion = 0
+    
+    for modelo_id in stock:
+
+        if(modelo_id == modelo):
+
+            stock[modelo_id][0] = p
+
+            actualizacion += 1
+            
+    if (actualizacion == 0):
+
+        return(False)        
+        
+    else:
+
+        return(True)
+
--- main.py
+++ main.py
@@ -1,25 +1,188 @@
 # === PROGRAMA PRINCIPAL === #
 import functions as func
 
-print("\n === ** Tienda Online Pybook ** ===")
+# === DICCIONARIOS === #
 
 productos = {
-    "8475HD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
-    "2175HD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
-    "JjfFHD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
-    "fgdxFHD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
-    "GF75HD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
-    "123FHD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
-    "342FHD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
-    "UWU131HD" : ["HP" , "15.6" , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"]
+    "8475HD" : ["hp" , 15.6 , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"],
+    "2175HD" : ["lenovo" , 14 , "4GB" , "SSD" , "512GB" , "Intel Core i5" , "Nvidia GTX1050"],
+    "JjfFHD" : ["asus" , 14 , "16GB" , "SSD" , "256GB" , "Intel Core i7" , "Nvidia GTX2080Ti"],
+    "fgdxFHD" : ["hp" , 15.6 , "8GB" , "DD" , "1T" , "Intel Core i3" , "integrada"],
+    "GF75HD" : ["asus" , 15.6 , "8GB" , "DD" , "1T" , "Intel Core i7" , "Nvidia GTX1050"],
+    "123FHD" : ["lenovo" , 14 , "6GB" , "DD" , "1T" , "AMD Ryzen 5" , "integrada"],
+    "342FHD" : ["lenovo" , 15.6 , "8GB" , "DD" , "1T" , "AMD Ryzen 7" , "Nvidia GTX1050"],
+    "UWU131HD" : ["Dell" , 15.6 , "8GB" , "DD" , "1T" , "AMD Ryzen 3" , "Nvidia GTX1050"],
+    "FS1230HD" : ["lenovo" , 15.6 , "8GB" , "DD" , "1T" , "Intel Core i5" , "Nvidia GTX1050"]
 }
 
 stock = {
     "8475HD" : [387990 , 10],
-    "2175HD" : [387990 , 10],
-    "JjfFHD" : [387990 , 10],
-    "GF75HD" : [387990 , 10],
-    "123FHD" : [387990 , 10],
-    "342FHD" : [387990 , 10],
-    "UWU131HD" : [387990 , 10]
+    "2175HD" : [327990 , 4],
+    "JjfFHD" : [424990 , 1],
+    "fgdxFHD" : [664990 , 21],
+    "GF75HD" : [749990 , 2],
+    "123FHD" : [290890 , 32],
+    "342FHD" : [444990 , 7],
+    "UWU131HD" : [349990 , 1],
+    "FS1230HD" : [249990 , 0]
 }
+
+# === MENÚ PRINCIPAL === #
+
+while True:
+
+    print("\n === ** Tienda Online Pybook ** ===")
+
+    print(""" 
+========================
+*** MENÚ PRINCIPAL ***
+1. Stock marca.
+2. Búsqueda por precio.
+3. Actualizar precio.
+4. Salir
+========================
+    """)
+
+    # === INGRESO DE OPCIONES === #
+
+    while True:
+
+        try:
+
+            opc = int(input("\nIngrese una opción (1-4): "))
+
+            if (opc >= 1 and opc <= 4):
+
+                break
+            
+            else:
+
+                print("\nERROR: Opción invalida. La opción debe ser un número del 1 al 4")
+
+        except(ValueError):
+
+            print("\nEERROR: Tipo de dato invalido. Debe ser un número entero.")
+
+    # === OPCIONES : === #
+
+    # == Stock por marca == #
+
+    if(opc == 1):
+
+        print("\n== ** Ver Stock por Marca ** ==")
+
+        marca = input ("\nIngrese marca del notebook: ").lower()
+
+        func.stock_marca(marca , productos , stock)
+
+    # == Búsqueda por precio == #
+
+    elif(opc == 2):
+
+        print("\n== ** Búsqueda por precio ** ==")
+
+        while True:
+
+            try:
+
+                p_min = int(input("\nIngrese precio mínimo: "))
+
+                if (p_min > 0):
+
+                    break
+                
+                else:
+
+                    print("\nERROR: El precio no puede ser un número negativo.")
+
+            except(ValueError):
+
+                print("\nEERROR: Tipo de dato invalido. Debe ser un número entero.")
+
+        while True:
+
+            try:
+
+                p_max = int(input("\nIngrese precio máximo: "))
+
+                if (p_max > 0):
+
+                    break
+                
+                else:
+
+                    print("\nERROR: El precio no puede ser un número negativo.")
+
+            except(ValueError):
+
+                print("\nEERROR: Tipo de dato invalido. Debe ser un número entero.")
+
+        func.busqueda_precio(p_min , p_max , stock , productos)
+
+    # == Actualizar precio == #
+
+    elif (opc == 3):
+
+        print("\n== ** Actualizar precio ** ==")
+
+        while True:
+            
+            modelo = input("\nIngrese el nombre del modelo: ")
+
+            while True:
+
+                try:
+
+                    p = int(input("\nIngrese precio nuevo: "))
+
+                    if (p > 0):
+
+                        break
+                    
+                    else:
+
+                        print("\nERROR: El precio no puede ser un número negativo.")
+
+                except(ValueError):
+
+                    print("\nEERROR: Tipo de dato invalido. Debe ser un número entero.")
+
+            if(func.actualizar_precio(modelo , p , stock)):
+
+                print("\n¡Precio actualizado exitosamente!")
+
+                print("\nAquí esta el stock actualizado con el nuevo precio: ")
+
+                for modelo in stock:
+
+                    print(f"\nModelo {modelo} : {stock[modelo]}")
+
+            else:
+
+                print("\nParece que el modelo no existe.")
+
+            while True:
+
+                valido = ["sí" , "si", "s" , "no", "n"]
+
+                respuesta = input("\n¿Desea actualizar otro precio? (si/no): ").lower()
+
+                if (respuesta in valido):
+
+                    break
+
+                else:
+
+                    print("\nERROR: Respuesta invalida. Debe responder si o no.")
+
+            if (respuesta == "no" or respuesta == "n"):
+
+                break
+
+    # == Salir de programa == #
+
+    else:
+
+        print("\nPrograma finalizado.")
+
+        break
